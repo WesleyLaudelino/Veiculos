7edd60d20147f04494508b4af843d72eee7fca80
Wesley Silva <wesleylaudelino@outlook.com>
2017-09-24 16:19:33 -0400

    Adicionar arquivos de projeto.

---
+++ Veiculos/Models/Veiculo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Veiculos.Models
+{
+    public class Veiculo
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public string AnoFabricacao { get; set; }
+        public string AnoModelo { get; set; }
+    }
+}
